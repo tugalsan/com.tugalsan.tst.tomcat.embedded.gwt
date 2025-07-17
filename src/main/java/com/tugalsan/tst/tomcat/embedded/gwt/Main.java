214d5689780b94951c27fa2501dcecf5a03d8688
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-07-17 09:16:34 +0300

    .

--- src/main/java/com/tugalsan/tst/tomcat/embedded/gwt/Main.java
+++ src/main/java/com/tugalsan/tst/tomcat/embedded/gwt/Main.java
@@ -1,5 +1,6 @@
 package com.tugalsan.tst.tomcat.embedded.gwt;
 
+import com.tugalsan.api.log.server.TS_Log;
 import com.tugalsan.api.thread.server.sync.TS_ThreadSyncTrigger;
 import com.tugalsan.api.tomcat.embedded.gwt.server.*;
 import com.tugalsan.api.tomcat.embedded.gwt.server.servlets.*;
@@ -7,11 +8,13 @@ import com.tugalsan.api.tomcat.embedded.gwt.server.utils.*;
 
 public class Main {
 
+    private final static TS_Log d = TS_Log.of(Main.class);
+
     //HOW TO EXECUTE
     //C:\me\codes\com.tugalsan\tst\com.tugalsan.tst.tomcat.embedded.gwt>target\bin\webapp.bat
     public static void main(String[] args) {
         var tomcatBall = TS_TomcatBall.of(
-                TS_ThreadSyncTrigger.of(),
+                TS_ThreadSyncTrigger.of(d.className + ".main"),
                 "",
                 servlets -> {
                     servlets.add(new TS_ServletAliveByMapping());
